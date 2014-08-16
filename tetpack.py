8ffa9e35c5f3ba39bd6db54a3685c8bfdf14111f
rhsimplex <hendersonry@gmail.com>
2014-08-16 21:10:55 +0200

    added get tets method

--- tetpack.py
+++ tetpack.py
@@ -11,7 +11,8 @@ def n_nearest_neighbors(structure, n, max_dist = 5.0):
 def simplex_volume(sites):
     return abs(np.dot((sites[0].coords - sites[3].coords), np.cross(sites[1].coords-sites[3].coords, sites[2].coords-sites[3].coords) ))/6.0
 
-def get_tets(structure, site, neighbors):    
+def get_tets(structure, site, neighbors, stdcutoff = 0.10, volcutoff = 2.0):#relax stdcutoff to 0.50 for more initial tetrahedra
+    tets=[]
     for face in combinations(neighbors, 3):
         edge_distances = [
                 site.distance_from_point(face[0].coords),
@@ -21,5 +22,9 @@ def get_tets(structure, site, neighbors):
                 face[0].distance_from_point(face[2].coords),
                 face[1].distance_from_point(face[2].coords)
                 ]
-        print np.std(edge_distances), simplex_volume( (site,) + face )
+        dstd = np.std(edge_distances)
+        v = simplex_volume( (site,) + face )
+        if dstd < stdcutoff and v > volcutoff:
+            tets.insert(-1, (site,) + face)            
+    return tets
 
